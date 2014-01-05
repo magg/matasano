206e8786a5211f9690443ed0b07a2c01a7ac8739
Miguel Gonzalez <maggonzz@gmail.com>
2014-01-04 20:42:41 -0600

    problem 4 was added

---
+++ detect_xor.rs
@@ -0,0 +1,15 @@
+/*
+4. Detect single-character XOR
+
+One of the 60-character strings at:
+
+  https://gist.github.com/3132713
+
+has been encrypted by single-character XOR. Find it. (Your code from
+#3 should help.)
+
+*/
+
+fn main () {
+	
+}
