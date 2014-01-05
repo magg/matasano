c8b47428087047a4e89cce87bd80d10b9f06da59
Miguel Gonzalez <maggonzz@gmail.com>
2014-01-04 18:29:51 -0600

    Some comments added

--- fixed_xor.rs
+++ fixed_xor.rs
@@ -1,3 +1,23 @@
+/*
+2. Fixed XOR
+
+Write a function that takes two equal-length buffers and produces
+their XOR sum.
+
+The string:
+
+ 1c0111001f010100061a024b53535009181c
+
+... after hex decoding, when xor'd against:
+
+ 686974207468652062756c6c277320657965
+
+... should produce:
+
+ 746865206b696420646f6e277420706c6179
+
+*/
+
 extern mod extra;
 extern mod std;
 use std::{u8};
@@ -25,13 +45,11 @@ fn xor_vectors(string1: ~str, string2: ~str) {
 	
 	let tam = xs1.len();
 	while i < tam {
-		//println(i.to_str() ); 		
-
-		let temp1 = u8::parse_bytes(xs1.slice(i, i+2), 16);
-		let temp2 = u8::parse_bytes(xs2.slice(i, i+2), 16);
-	
 		
-		match (temp1, temp2) {
+		let slice1 = u8::parse_bytes(xs1.slice(i, i+2), 16);
+		let slice2 = u8::parse_bytes(xs2.slice(i, i+2), 16);
+	
+		match (slice1, slice2) {
 		      (Some(a), Some(b)) => {
 						let x: u8 = a ^ b;
 						let y: ~str = x.to_str_radix(16);
